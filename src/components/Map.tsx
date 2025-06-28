fc17ea0eea3d850b7e67aca50b421ea029ae619b
Alex Raymond <alexjacobraymond@gmail.com>
2025-06-28 15:11:04 -0700

    updated map component

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -5,7 +5,7 @@
      return (
        <div className="map-container">
          <img src="/maps/concealed_hill.png" alt="Concealed Hill" className="map-image" />
-         <div className="circle" style={{ top: '50px', left: '50px' }}></div>
+         <div className="circle" style={{ top: '550px', left: '300px' }}></div>
          <div className="circle" style={{ top: '150px', left: '120px' }}></div>
          {/* This is where mroe circles go */}
        </div>
