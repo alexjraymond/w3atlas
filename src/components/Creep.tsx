a7d6dd22a1ec568c8ff0fe74ecc22ebd5c85eb66
Alex Raymond <alexjacobraymond@gmail.com>
2025-06-29 13:33:44 -0700

    made colors nicer

---
+++ src/components/Creep.tsx
@@ -0,0 +1,10 @@
+function Creep() {
+
+
+
+    return (  
+    <></>
+);
+}
+
+export default Creep;
--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -5,8 +5,8 @@
      return (
        <div className="map-container">
          <img src="/maps/concealed_hill.png" alt="Concealed Hill" className="map-image" />
-         <div className="circle" style={{ top: '550px', left: '300px' }}></div>
-         <div className="circle" style={{ top: '150px', left: '120px' }}></div>
+         <div className="circle" style={{ top: '520px', left: '275px' }}></div>
+         <div className="circle" style={{ top: '20px', left: '275px' }}></div>
          {/* This is where mroe circles go */}
        </div>
      );
